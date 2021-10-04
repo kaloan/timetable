c61ad344de2a300798b2b6c07d46b6bfddd7ee6e
Калоян Стоилов <kkstoilov@uni-sofia.bg>
2021-10-04 23:42:30 +0300

    Add local storage and comments

--- timetable.js
+++ timetable.js
@@ -5,8 +5,6 @@
   ["Анализ3(л)", 1, 12, 16]
 ];*/
 
-console.log(subjects);
-
 const days = [
   "Понеделник",
   "Вторник",
@@ -31,8 +29,20 @@ subjects.forEach((item, i) => {
 
 const chosenList = document.getElementById("chosenList");
 
+let saveChoises = new Set();
 
-function createTable(){
+
+const shownDaysOfTheWeek = [1,2,3,4,5,6];
+//Start of first block
+const startingHour=8;
+//Start of last block
+const endingHour=21;
+//Block time length
+const blockLength=1;
+
+
+
+function createTable() {
   const timetable = document.getElementById("timetable");
 
   //Trix2g
@@ -42,15 +52,14 @@ function createTable(){
   timetable.appendChild(upperHole);
 
   //Fill hours on top
-  for(let j=8; j<=21; j++)
-  {
+  for(let j=8; j<=21; j++) {
     const hour = document.createElement("div");
     hour.classList.add("time");
     hour.appendChild(document.createTextNode(`${j}:00-${j+1}:00`));
     timetable.appendChild(hour);
   }
 
-  for(let i=1; i<=6; i++) {
+  shownDaysOfTheWeek.forEach(i => {
     let arr = [];
 
     //Fill days on left side
@@ -60,17 +69,16 @@ function createTable(){
     timetable.appendChild(day);
 
     //Create timeblocks
-    for(let j=8; j<=21; j++) {
+    for(let j=startingHour; j<=endingHour; j+=blockLength) {
       const block = document.createElement("div");
       block.classList.add("block");
       block.setAttribute("day", i);
       block.setAttribute("hour", j);
-      //block.appendChild(document.createTextNode(`(Day: ${i}, hour start: ${j})`));
       timetable.appendChild(block);
       arr.push(block);
     }
     blocks.push(arr);
-  }
+  });
 
   //Trix2g returns
   const lowerHole = document.createElement("div");
@@ -79,8 +87,7 @@ function createTable(){
   timetable.appendChild(lowerHole);
 
   //Fill hours on bottom
-  for(let j=8; j<=21; j++)
-  {
+  for(let j=startingHour; j<=endingHour; j+=blockLength) {
     const hour = document.createElement("div");
     hour.classList.add("time");
     hour.appendChild(document.createTextNode(`${j}:00-${j+1}:00`));
@@ -89,9 +96,6 @@ function createTable(){
 }
 
 function main() {
-  //const subjects = JSON.parse(subjs);
-  //console.log(subjects);
-
   createTable();
 
   const search = document.getElementById("search");
@@ -102,13 +106,22 @@ function main() {
   go.addEventListener('click', (event) => {
     listLike(search.value);
   });
+
+  const locallySavedChoices = localStorage.getItem('saveChoises');
+  if(locallySavedChoices) saveChoises = new Set(JSON.parse(locallySavedChoices));
+
+  saveChoises.forEach((subjectId) => {
+    choose(subjectId);
+  });
 }
 
 function listLike(str) {
+  //Clear current search list
   const searchList = document.getElementById("searchList");
   searchList.replaceChildren();
 
   subjects.forEach((subject, index) => {
+    //Only show the subjects not already chosen
     if(subject[0].toLowerCase().includes(str.toLowerCase()) && !document.getElementById(`chosenListSubjectId-${index}`)){
       const subjNode = document.createElement("div");
       subjNode.classList.add("choice");
@@ -119,9 +132,8 @@ function listLike(str) {
       subjNode.addEventListener("click", (event) => {
         const actualIndex = event.target.getAttribute("subjectId");
         choose(actualIndex);
+        //Get rid of the item on the search list
         searchList.removeChild(event.target);
-        //if(chosen[actualIndex].length == 0) choose(actualIndex);
-        //else unchoose(actualIndex);
       });
 
       searchList.appendChild(subjNode);
@@ -133,16 +145,19 @@ function listLike(str) {
 function choose(index) {
   const chosenSubject = subjects[index];
 
+  //Add the info to the timetable
   for(let j=chosenSubject[2]; j<chosenSubject[3];j++) {
     //-1 for day and -8 for hour
-    const block = blocks[chosenSubject[1]-1][j-8];
+    const block = blocks[chosenSubject[1]-1][j-startingHour];
     const subjNode = document.createElement("div");
     subjNode.classList.add("subj");
     subjNode.appendChild(document.createTextNode(chosenSubject[0]));
     block.appendChild(subjNode);
 
+    //Save the HTML nodes that should be cleared
     chosen[index].push(subjNode);
 
+    //Show the conflict in time
     if(block.childNodes.length != 1){
       block.classList.add("conflict");
       conflicts.push({day: days[chosenSubject[1]-1], hour: chosenSubject[2]});
@@ -150,27 +165,32 @@ function choose(index) {
     else block.classList.add("chosen");
   }
 
+  //Insert the subejct in the list of chosen subjects
   const selNode = document.createElement("div");
   selNode.classList.add("choice");
   selNode.appendChild(document.createTextNode(`${chosenSubject[0]}, ${days[chosenSubject[1]-1]} ${chosenSubject[2]}-${chosenSubject[3]}`));
   selNode.setAttribute("subjectId", index);
   selNode.id = `chosenListSubjectId-${index}`;
+
   selNode.addEventListener("click", (event) => {
     const actualIndex = event.target.getAttribute("subjectId");
     unchoose(actualIndex);
   });
   chosenList.appendChild(selNode);
 
-  //searchList.removeChild(document.getElementsByTagName(`possibleListSubjectId-${index}`));
+  //Save new choises to local storage
+  localStorage.setItem('saveChoises', JSON.stringify(Array.from(saveChoises.add(index))));
 }
 
 function unchoose(index){
-  console.log(chosen);
+  //Delete the HTML nodes
   chosen[index].forEach((item, i) => {
       const parent = item.parentNode;
       if(parent){
+        //Change the css class of the block
         if(parent.childNodes.length == 2) parent.classList.remove("conflict");
         else if (parent.childNodes.length == 1) parent.classList.remove("chosen");
+        //Remove the subject from the timetable
         item.parentNode.removeChild(item);
       }
   });
@@ -178,7 +198,12 @@ function unchoose(index){
 
   const nodeToDelInChosenList = document.getElementById(`chosenListSubjectId-${index}`);
   chosenList.removeChild(nodeToDelInChosenList);
+
+  //Refresh the choise list
   document.getElementById("go").click();
+
+  //Save new choises to local storage
+  localStorage.setItem('saveChoises', JSON.stringify(Array.from(saveChoises.delete(index))));
 }
 
 main();
